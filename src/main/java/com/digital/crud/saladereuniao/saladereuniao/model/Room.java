c21b99eb44ac966e6789feae376c400c1228a03c
Juliocbs04 <profile.julio@gmail.com>
2021-09-05 15:28:36 -0300

    Aula 03 - Criando as anotações de Entity e Table, criando as colunas e anotações referentes a room

--- src/main/java/com/digital/crud/saladereuniao/saladereuniao/model/Room.java
+++ src/main/java/com/digital/crud/saladereuniao/saladereuniao/model/Room.java
@@ -1,5 +1,39 @@
 package com.digital.crud.saladereuniao.saladereuniao.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "meetingroom")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Room {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    @Column(name="name", nullable = false)
+    private String name;
+    @Column(name="date", nullable = false)
+    private String date;
+    @Column(name="startHour", nullable = false)
+    private String startHour;
+    @Column(name="endHour", nullable = false)
+    private String endHour;
+
+    @Override
+    public String toString() {
+        return "Room{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", date='" + date + '\'' +
+                ", startHour='" + startHour + '\'' +
+                ", endHour='" + endHour + '\'' +
+                '}';
+    }
 }
